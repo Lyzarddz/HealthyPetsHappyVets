31aa50d7c4999592ef55b3775648a20be40179ab
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-19 15:27:32 -0500

    added sessions controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
   
   resources :records
-  resources :vets, only:[index, show, create]
-  resources :pets, only:[index, create, show, destroy]
-  resources :owners, only: [index, show, create]
+  resources :vets, only: [:index, :show, :create]
+  resources :pets, only: [:index, :create, :show, :destroy]
+  resources :owners, only: [:index, :show, :create]
 
   post "/signup", to: "owners#create"
   get "/me", to:"owners#show"
