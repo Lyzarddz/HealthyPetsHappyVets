807f7a612c9450d1af4f867cc858c9448bf75a26
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-19 15:50:44 -0500

    added create and destroy methods to sessions cont

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,17 @@
 class SessionsController < ApplicationController
+
+    def create 
+        owner= Owner.find_by(username: params[:username])
+        if owner&.authenticate(params[:password])
+            session[:owner_id] = owner.index
+            render json: owner
+        else
+            render json: {errors:["Invalid username or password"]}, status: :unauthorized
+        end
+    end
+
+    def destroy
+        session.delete(:owner_id)
+        head :no_content
+    end
 end
