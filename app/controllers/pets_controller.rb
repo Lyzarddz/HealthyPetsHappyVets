61f42e4088a00295bf34c3162873fc00215771d7
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-21 13:15:03 -0500

    Completed pet controller

--- app/controllers/pets_controller.rb
+++ app/controllers/pets_controller.rb
@@ -1,2 +1,28 @@
 class PetsController < ApplicationController
+
+    def index 
+        render json: Pet.all, status: :ok
+    end
+
+    def show 
+        pet = Pet.find(params[:id])
+        render json: pet
+    end
+
+    def create
+        pet = Pet.create!(pet_params)
+        render json: pet, status: :created
+    end
+
+    def destroy
+        pet = Pet.find(params[:id])
+        pet.destroy
+        head :no_content
+    end
+
+    private
+
+    def pet_params
+        params.permit(:name, :species, :age, :vet_id, :owner_id)
+    end
 end
