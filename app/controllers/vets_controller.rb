d3d777419ff835b7ad08be9a2c7777da67e065b3
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-21 12:59:28 -0500

    added create to vet controller

--- app/controllers/vets_controller.rb
+++ app/controllers/vets_controller.rb
@@ -1,2 +1,20 @@
 class VetsController < ApplicationController
+
+    def index 
+
+    end
+
+    def show 
+    end
+
+    def create 
+        vet = Vet.create!(vet_params)
+        render json: vet, status: :created
+    end
+
+    private
+    
+    def vet_params
+        params.permit(name)
+    end
 end
