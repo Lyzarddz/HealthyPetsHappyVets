2e351f93f9aad56e0da25f19b69fcb2c4202d43f
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-21 13:10:25 -0500

    finished records controller

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,12 +1,29 @@
 class RecordsController < ApplicationController
 
     def index 
-        render json: Record.all
+        render json: Record.all, status: :ok
+    end
+
+    def show
+        record = Record.find(params[:id])
+        render json: record
     end
 
     def create
         record = Record.create!(record_params)
         render json: record, status: :created
+    end
+
+    def update
+        record = Record.find(params[:id])
+        record.update(record_params)
+        render json: record, status: :update
+    end
+    
+    def destroy
+        record = Record.find(params[:id])
+        record.destroy
+        head :no_content
     end
 
     private
