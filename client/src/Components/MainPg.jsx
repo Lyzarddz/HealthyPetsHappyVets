435fdfdbd5c96337ffb3f07549d19555a2f98b5a
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-23 16:41:20 -0500

    finished landing page

--- client/src/Components/MainPg.jsx
+++ client/src/Components/MainPg.jsx
@@ -1,9 +1,37 @@
+import { getRadioUtilityClass } from "@mui/material";
 
 
 const MainPg = () => {
+
+    const myStyles = {
+        backgroundImage: `url("https://user-images.githubusercontent.com/91993983/203647488-68f8e164-1def-4f46-aeb5-ea49ce683c76.jpeg")`,
+        height:'100vh',
+        backgroundSize: 'cover',
+        backgroundRepeat: 'no-repeat',
+        paddingLeft: 40,
+        paddingTop: 40.
+    }
     return (
         
-<h1>From mainpg</h1>
+
+        <div style={myStyles} className="largeText"> 
+            No matter the pet, 
+            <br/>
+            We are here to help 
+            <br/>
+            keep track of their 
+            <br/> 
+            health!
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <br/>
+            <div className="smallText">
+            Signup or Login to begin!
+            </div>
+          
+          </div>
 
     )
 }
