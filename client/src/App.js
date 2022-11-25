b79ff8a52c0cc6d1bbe10f2952b0f7a198e8741a
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-25 13:23:20 -0500

    added conditional rendering for main pg-logged in/logged out

--- client/src/App.js
+++ client/src/App.js
@@ -11,7 +11,7 @@ import Signup from './Components/Signup';
 function App() {
 
   const [currentUser, setCurrentUser] = useState({});
-  const [loggedIn, setLoggedIn]  = useState(false);
+  const [loggedIn, setLoggedIn]  = useState(true);
   const [errors, setErrors] = useState([]);
 
   function loginUser (user) {
@@ -50,7 +50,7 @@ function App() {
     <NavBar loggedIn={loggedIn} logoutUser={logoutUser}/>
     <Errors errors= {errors} />
     <Routes>
-     <Route path="/" element= {<MainPg loginUser={loginUser}/>} />
+     <Route path="/" element= {<MainPg loggedIn={loggedIn}/>} />
      <Route path="/login" element= {<Login clearErrors={ clearErrors } loginUser={loginUser} addErrors= {addErrors}/>} />
      <Route path="/signup"  element= {<Signup clearErrors={ clearErrors } loginUser={loginUser} addErrors= {addErrors} />} />
      <Route path="/createRecord"  element= {<CreateRecord clearErrors={ clearErrors } addErrors= {addErrors} />} />
--- client/src/Components/MainPg.jsx
+++ client/src/Components/MainPg.jsx
@@ -1,7 +1,7 @@
 
 
 
-const MainPg = ({ loginUser}) => {
+const MainPg = ({ loggedIn }) => {
 
     const myStyles = {
         backgroundImage: `url("https://user-images.githubusercontent.com/91993983/203647488-68f8e164-1def-4f46-aeb5-ea49ce683c76.jpeg")`,
@@ -52,27 +52,9 @@ const MainPg = ({ loginUser}) => {
       }
 
     return (
-        
-
-        <div style={myStyles} className="largeText"> 
-            No matter the pet, 
-            <br/>
-            We are here to help 
-            <br/>
-            keep track of their 
-            <br/> 
-            health!
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <br/>
-            <div className="smallText">
-            Signup or Login to begin!
-            </div>
-          
-          </div>
-
+        <div>
+             { loggedIn ? loggedInDisplay() :  loggedOutDisplay()}
+        </div>
     )
 }
 
