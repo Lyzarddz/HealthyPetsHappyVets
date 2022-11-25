45364f108122816bed3c48d4d0fad50e8ae0d293
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-25 17:41:55 -0500

    added pet add form

--- client/src/Components/CreatePet.jsx
+++ client/src/Components/CreatePet.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { Form } from "semantic-ui-react";
 import { useNavigate } from 'react-router-dom';
 
-const CreatePet = () => {
+const CreatePet = ({ addPet }) => {
 
 
   const navigate = useNavigate();
@@ -10,11 +10,82 @@ const CreatePet = () => {
     name: "",
     species: "",
     age: "",
+    vet: "",    
   });
 
-    return (
-        <h1>CreatePet</h1>
-    )
+  function handleChange(event) {
+    setFormData({
+      ...formData,
+      [event.target.name]: event.target.value,
+    });
+  }
+
+
+function handleSubmit() {
+
+  const newPet = {
+    name: formData.name,
+    species: formData.species,
+    age: formData.age,
+    vet: formData.vet
+ };
+
+
+ fetch("http://localhost:3000/pets", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(newPet),
+  })
+    .then((r) => r.json())
+    .then(addPet);
+    navigate("/");
 }
+
+    return (
+         <div className='primary'>
+      <h1 >Add a Pet!</h1>
+      <Form onSubmit={handleSubmit}>
+        <Form.Group widths="equal">
+          <Form.Input
+            label="Name"
+            placeholder="Name"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+          <br></br>
+          <Form.Input
+            label="Species"
+            placeholder="Species"
+            name="species"
+            value={formData.species}
+            onChange={handleChange}
+          />
+           <br></br>
+          <Form.Input
+            label="Age"
+            placeholder="Age"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+          />
+           <br></br>
+          <Form.Input
+            label="Vet"
+            placeholder="Vet"
+            name="vet"
+            value={formData.vet}
+            onChange={handleChange}
+          />
+           <br></br>
+        </Form.Group>
+        <Form.Button className="btn">Submit</Form.Button>
+      </Form>
+    </div>
+    )
+    }
+
 
 export default CreatePet;
