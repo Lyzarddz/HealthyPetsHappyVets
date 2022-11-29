47ec42f9a052103b8ed4bc152dc6b640a947ffb0
Alyssa Falcione <lyzarddz@gmail.com>
2022-11-29 16:14:56 -0500

    added memoization to currednt owner

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,8 @@ rescue_from ActiveRecord::RecordNotFound, with: :not_found_resp
 
 before_action :authorize
 
+
+
 private 
 
 def unproccesable_entity_resp(exception)
@@ -17,10 +19,8 @@ def not_found_resp
 end
 
 def authorize
-  @current_owner = Owner.find_by(id: session[:owner_id])
+  @current_owner ||= Owner.find_by(id: session[:owner_id])    #memoization
   render json: {errors:["Not Authorized"]}, status: :unauthorized unless @current_owner
 end
 
 end
-+
-3.
