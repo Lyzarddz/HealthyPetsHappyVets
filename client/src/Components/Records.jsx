4fa95f681dd2ae3a0e26b57a76eca0e75d212c69
Alyssa Falcione <lyzarddz@gmail.com>
2022-12-02 18:11:34 -0500

    added records page styling

--- client/src/Components/Records.jsx
+++ client/src/Components/Records.jsx
@@ -1,21 +1,39 @@
 import React, { useState } from 'react';
 import { Form } from "semantic-ui-react";
-import { useNavigate } from 'react-router-dom';
+import Card from '@mui/material/Card';
+import Button from '@material-ui/core/Button';
+import { Link, useNavigate } from 'react-router-dom';
+import DeleteIcon from '@mui/icons-material/Delete';
 
-const Records = () => {
+
+const Records = ( ) => {
 
     const navigate = useNavigate();
-
-    const [formData, setFormData] = useState({
-        vaccine: "",
-        prevention: "",
-        altered: "",
-      });
-
-    return (
-        <h1>Records</h1>
-
-    )
+  return (
+    <Card variant="outlined">
+        
+        <div className="content" >
+        </div>
+        <h3>
+        <div className="extra content" >
+       
+            <ul>
+          Vaccine(s):
+              <br/>
+            Prevention:
+            <br></br>
+            Altered?:
+           <br></br>
+           Notes:
+           <br/>
+           <br/>
+           <DeleteIcon />
+            </ul>
+        </div>
+    
+        </h3>
+    </Card>
+  )
 }
 
 export default Records;
