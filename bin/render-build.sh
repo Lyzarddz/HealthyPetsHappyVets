230f269821c230590cf30bff79abba25d7bc0377
Alyssa Falcione <lyzarddz@gmail.com>
2022-12-30 16:24:57 -0500

    another attempt to fix deployment

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -3,6 +3,13 @@
 # exit on error
 set -o errexit
 
+
+# Add build commands for front end
+rm -rf public
+npm install --prefix client && npm run build --prefix client
+cp -a client/build/. public/
+
+
 bundle install
 # bundle exec rake assets:precompile # These lines are commented out because we have an API only app
 # bundle exec rake assets:clean
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,4 @@
 Rails.application.routes.draw do
-  namespace :api do
 # root 'sessions#home'
 
   resources :records
@@ -14,7 +13,7 @@ Rails.application.routes.draw do
   delete "/logout", to:"sessions#destroy"
   get "/vets", to:"vets#show"
   get "/records/:id", to:"records#show"
-end
+
 
   # Routing logic: fallback requests for React Router.
   # Leave this here to help deploy your app later!
